2ad28da63d45a943a6b5d5ad2b25a29e27c4714e
jarett <jarett@wustl.edu>
2016-06-17 18:40:01 -0400

    more styling for each section

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
 var isRightArrow = true;
-var arrowWidth = (2420 / document.documentElement.clientWidth) + "vw";
+var isNavReset = true;
 
 $(document).ready(function() {
 	var myIcons = new SVGMorpheus("#svg-controller");
 	
 	$("#svg-controller").click(function() {
-		if (isRightArrow) {
+		if (isRightArrow) { //close navbar
+			var arrowWidth = 2 + (2380 / document.documentElement.clientWidth) + "vw";
 			myIcons.to("right-arrow", {duration: 1000, easing: "linear", rotation: "none"});	
-			jQuery(".nav").animate({ width: arrowWidth, right: "0%" }, 1000, "swing");
-			$("#nav-top").css("display", "none");
-			$(".nav-wrapper").css("display", "none");
-		} else {
+			jQuery(".nav").animate({ width: arrowWidth }, 1000, "swing", function() {
+				$(".nav-wrapper").css("transform", "translateX(0%)");
+				$(".nav-wrapper").css("-webkit-transform", "translateX(0%)");
+				$(".nav-wrapper").css("-moz-transform", "translateX(0%)");
+				$(".nav-wrapper").css("-o-transform", "translateX(0%)");
+			});
+		} else { //open navbar
 			myIcons.to("left-arrow", {duration: 1000, easing: "linear", rotation: "none"});
-			jQuery(".nav").animate({ width: "100%", right: "0%" }, 1000, "swing");
-			$("#nav-top").css("display", "inline");
-			$(".nav-wrapper").css("display", "inline-flex");
+			jQuery(".nav").animate({ width: "100%" }, 1000, "swing");
+			$(".nav-wrapper").css("transform", "translateX(-50%)");
+			$(".nav-wrapper").css("-webkit-transform", "translateX(-50%)");
+			$(".nav-wrapper").css("-moz-transform", "translateX(-50%)");
+			$(".nav-wrapper").css("-o-transform", "translateX(-50%)");
+			isNavReset = false;
 		}
 		isRightArrow = !isRightArrow;
-	}); 
+	});
+	
+	$(document).scroll(function() {
+		if (document.documentElement.scrollTop != 0 && isRightArrow && isNavReset) {
+			var arrowWidth = 2 + (2380 / document.documentElement.clientWidth) + "vw";
+			myIcons.to("right-arrow", {duration: 1000, easing: "linear", rotation: "none"});	
+			jQuery(".nav").animate({ width: arrowWidth }, 1000, "swing", function() {
+				$(".nav-wrapper").css("transform", "translateX(0%)");
+				if (document.documentElement.scrollTop == 0) {
+					$(".nav-wrapper").css("transform", "translateX(-50%)");
+					$(".nav-wrapper").css("-webkit-transform", "translateX(-50%)");
+					$(".nav-wrapper").css("-moz-transform", "translateX(-50%)");
+					$(".nav-wrapper").css("-o-transform", "translateX(-50%)");
+				}
+			});
+			isRightArrow = !isRightArrow;
+		} else if (document.documentElement.scrollTop == 0 && !isRightArrow) {
+			myIcons.to("left-arrow", {duration: 1000, easing: "linear", rotation: "none"});
+			jQuery(".nav").animate({ width: "100%" }, 1000, "swing");
+			$(".nav-wrapper").css("transform", "translateX(-50%)");
+			isRightArrow = !isRightArrow;
+		}
+		if (document.documentElement.scrollTop == 0) {
+			isNavReset = true;
+		}
+	});
+	
+	$(window).resize(function(){
+		if (!isRightArrow) {
+			var arrowWidth = 2 + (2380 / document.documentElement.clientWidth) + "vw";
+			$(".nav").css("width", arrowWidth);
+		}
+	});
+	
+	$("#scroll-down-button").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#about").offset().top
+		}, 1000);
+	});
 });
